183bfb8da18214ee0d2dc785a9bcd7fa7375604d
Rodrigo Gregori <rodrigo.gregori@catolicasc.org.br>
2020-08-19 17:59:49 -0700

    Primeiro programa java

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+	public static void main(String[] args)
+	{
+		System.out.print("Esta e uma saudacao: ");
+		System.out.println("Hello, world!");
+		System.out.printf("Idade: %d\n", 20);
+	}
+}
+
+// javac Hello.java - compila para bytecode (.class)
+// java Hello - executa o arquivo .class
